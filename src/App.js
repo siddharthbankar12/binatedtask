30fd8d849ec91f0fe8e43921452bfa6aa0c0399c
Siddharth Bankar <siddharthbankar1204@gmail.com>
2025-02-24 22:37:46 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Tictactoe from "./Tictactoe";
 import "./App.css";
 import Home from "./Home";
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Tictactoe />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Tictactoe />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
